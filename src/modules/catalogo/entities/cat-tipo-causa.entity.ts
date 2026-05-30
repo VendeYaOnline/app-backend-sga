35ab13009deed44ce642da2d58b997494439b4b3
Rosyec Parrado <chechomens@gmail.com>
2026-05-30 09:09:34 -0500

    chore: actualizar pnpm-lock (multer types) y reformatear CatTipoCausa

--- src/modules/catalogo/entities/cat-tipo-causa.entity.ts
+++ src/modules/catalogo/entities/cat-tipo-causa.entity.ts
@@ -1,8 +1,4 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity('sga.CAT_TIPO_CAUSA')
 export class CatTipoCausa {
